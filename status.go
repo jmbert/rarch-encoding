2bdde67ff8c16b5ed8cd282036eb38f0b008d01d
Joe Berthels <joe2157b@gmail.com>
2024-01-06 14:52:39 +0000

    Added opcodes

--- status.go
+++ status.go
@@ -1,11 +1,20 @@
 package rarch_encoding
 
 const (
-	STATUS_CARRY   uint64 = iota
-	STATUS_ZERO           = 1 << iota
-	STATUS_CINDEX         = 2 << iota
-	STATUS_OFFSIZE        = 6 << iota
-	STATUS_PARITY         = 8 << iota
-	STATUS_SIGN           = 9 << iota
-	STATUS_HALTED         = 10 << iota
+	STATUS_CARRY   uint64 = 1 << iota
+	STATUS_ZERO           = 1 << 1
+	STATUS_INDEX          = 1 << 2
+	STATUS_OFFSIZE        = 1 << 6
+	STATUS_PARITY         = 1 << 8
+	STATUS_SIGN           = 1 << 9
+	STATUS_HALTED         = 1 << 10
+)
+const (
+	STATUS_CARRY_BIT   uint64 = iota
+	STATUS_ZERO_BIT           = 1
+	STATUS_INDEX_BIT          = 2
+	STATUS_OFFSIZE_BIT        = 6
+	STATUS_PARITY_BIT         = 8
+	STATUS_SIGN_BIT           = 9
+	STATUS_HALTED_BIT         = 10
 )
